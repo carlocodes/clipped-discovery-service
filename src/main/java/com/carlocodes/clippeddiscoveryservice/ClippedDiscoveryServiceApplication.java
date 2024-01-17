4a7738d65a008be10d9b3147c85663a22eca0f8f
carlocodes <Gian Uy>
2024-01-17 22:12:15 +0800

    initial commit

---
+++ src/main/java/com/carlocodes/clippeddiscoveryservice/ClippedDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.carlocodes.clippeddiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClippedDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClippedDiscoveryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carlocodes/clippeddiscoveryservice/ClippedDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlocodes.clippeddiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClippedDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
